86e37efe8da2f6f1ea34d93a529f9b2c52065ed5
Ludovic Lardies <ludoviclrds@gmail.com>
2015-06-29 21:55:40 +0200

    clean master

--- backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-NOW=$(date +"%m-%d-%Y")
-cd /var/www/vhosts/ludovicl.fr/httpdocs
-
-git add -A
-git commit -m "Blog backup from $NOW"
-git push
--- data/statiques/001.a-propos.php
+++
@@ -1,6 +0,0 @@
-<p>Ce <strong>blog-notes</strong> est un endroit permettant à ludovicl de <strong>commenter</strong>, de <strong>partager</strong>, <strong>d&#8217;archiver</strong> ses découvertes sur l&#8217;internet. </p>
-
-<ul>
-<li>Ce blog est généré par <a href="http://www.pluxml.org" target="_blank">PluXml</a></li>
-<li>Thème : <a href="https://github.com/ludovicl/PluXml-theme-ludovicl.git" target="_blank">https://github.com/ludovicl/PluXml-theme-ludovicl.git</a> inspiré par <a href="http://pluxopolis.net/static1/themes" target="_blank">Skya</a></li>
-</ul>
